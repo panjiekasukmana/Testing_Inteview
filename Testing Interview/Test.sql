c47d4778d602012d28ed9a4a9544b313a558d6f2
panjiekasukmana <panjiekasukmana24@gmail.com>
2023-04-16 11:55:28 +0700

    Third commit

--- Testing Interview/Test.sql
+++ Testing Interview/Test.sql
@@ -1,12 +1,6 @@
 SELECT emp_name, emp_status 
 FROM tbl_employee;
 
-SELECT tbl_employee.emp_name, tbl_income.emp_income
-FROM tbl_employee JOIN tbl_income
-ON tbl_employee.emp_code = tbl_employee.emp_code
-WHERE tbl_employee.emp_status = 'R'
-
-
 SELECT e.emp_name, i.emp_income
 FROM tbl_employee as e JOIN tbl_income as i
 ON e.emp_code = i.emp_code
